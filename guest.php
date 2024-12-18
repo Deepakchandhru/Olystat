5940f9c7885dfade4a310e7c226fe64f3420990d
Deepak Chandhru N <122144025+Deepakchandhru@users.noreply.github.com>
2024-12-18 19:49:53 +0530

    Delete guest.php

--- guest.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-echo "<script> window.alert('Welcome to our website'); </script>";
-$_SESSION['name']='Guest';
-$_SESSION['mode']='guest';
-header("Location: home.php");
-?>
